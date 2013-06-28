fcd220e195eda22d6832ccc97bfe545dd15c99d5
omry <omry@yadan.net>
2013-06-28 08:38:59 +0100

    source headers

--- banana/src/net/yadan/banana/memory/IPrimitiveAccess.java
+++ banana/src/net/yadan/banana/memory/IPrimitiveAccess.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2013 Omry Yadan <omry@yadan.net>
+ * All rights reserved.
+ *
+ * See https://github.com/omry/banana/blob/master/BSD-LICENSE for licensing information
+ */
 package net.yadan.banana.memory;
 
 public interface IPrimitiveAccess {
