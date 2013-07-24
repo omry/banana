3af458c78b34b148509c9ca73762147af1f432f0
omry <omry@yadan.net>
2013-07-24 00:32:50 -0700

    updated example

--- examples/src/net/yadan/banana/map/FormattingMapElements.java
+++ examples/src/net/yadan/banana/map/FormattingMapElements.java
@@ -8,51 +8,57 @@ package net.yadan.banana.map;
 
 import net.yadan.banana.DebugLevel;
 import net.yadan.banana.Formatter;
+import net.yadan.banana.ICollection;
 import net.yadan.banana.memory.Buffer;
 import net.yadan.banana.memory.IBuffer;
 import net.yadan.banana.memory.IPrimitiveAccess;
 
-
 /**
- * This usage example demonstrates using a map where the key is a variable
- * length structure In addition, it demonstrates how to use a Formatter to make
- * debugging the map easier
- * 
  * Created by : omry Date: 6/29/13
  */
 public class FormattingMapElements {
+
+  /**
+   * This usage example demonstrates using a map where the value is a variable
+   * length structure. In addition, it demonstrates how to use a Formatter to
+   * make debugging the map easier. All Banana data structures that implements
+   * {@link ICollection} supports formatting using a Formatter, so you can reuse
+   * your formatters.
+   * 
+   * Lets say we want the following C like struct as the value of our map. Note
+   * that this is variable length struct.
+   * 
+   * <pre>
+   * struct person {
+   *  int age
+   *  struct name {
+   *    int size
+   *    char chars[]
+   *  }
+   * }
+   * 
+   *      
+   * 
+   * Since banana storage is always ints, a record for 13 year old Romeo would look like
+   * 
+   * ┌──0──╦──1──┬──2──┬──3──┬──4──┐
+   * │     ║       NAME  STRING    │
+   * │ AGE ╬─────╦─────────────────┤
+   * │     ║SIZE ║      CHARS      │
+   * ├─────╬─────╬─────┬─────┬─────┤
+   * │ 13  ║  5  ║ R|o │ m|e │ o|  │
+   * └─────╩─────╩─────┴─────┴─────┘
+   * 
+   * Note that each int contains two chars (Java chars are 16 bit values).
+   * </pre>
+   */
+
+  // Offset constants
+  final static int AGE_OFFSET = 0;
+  final static int NAME_SIZE_OFFSET = 1;
+  final static int NAME_CHARS_OFFSET = 2;
+
   public static void main(String[] args) {
-    /**
-     * Lets say we want the following C like struct as the value of our map.
-     * Note that this is variable length struct.
-     * 
-     * <pre>
-     * struct person {
-     *  int age
-     *  struct name {
-     *    int size
-     *    char chars[]
-     *  }
-     * }
-     * 
-     *      
-     * 
-     * Since banana storage is always ints, a record for 13 year old Romeo would look like
-     * 
-     * ┌──0──╦──1──┬──2──┬──3──┬──4──┐
-     * │     ║       NAME  STRING    │
-     * │ AGE ╬─────╦─────────────────┤
-     * │     ║SIZE ║      CHARS      │
-     * ├─────╬─────╬─────┬─────┬─────┤
-     * │ 13  ║  5  ║ R|o │ m|e │ o|  │
-     * └─────╩─────╩─────┴─────┴─────┘
-     * 
-     * Note that each int contains two chars (Java chars are 16 bit values).
-     * </pre>
-     */
-    final int AGE_OFFSET = 0;
-    final int NAME_SIZE_OFFSET = 1;
-    final int NAME_CHARS_OFFSET = 2;
 
     // initial number of blocks in the block allocator
     int maxBlocks = 100;
@@ -64,7 +70,14 @@ public class FormattingMapElements {
     // what factor?
     double growthFactor = 2.0;
     IHashMap map = new HashMap(maxBlocks, blockSize, growthFactor, loadFactor);
-    
+
+    // activate map debug level
+    map.setDebug(DebugLevel.DEBUG_CONTENT);
+
+    // you can use a formatter to make the map easier to debug by using your own
+    // logic to convert records to strings
+    map.setFormatter(new PersonFormatter());
+
     long key = 1000;
     char name1[] = "Romeo".toCharArray();
 
@@ -84,20 +97,6 @@ public class FormattingMapElements {
     map.setInt(r, NAME_SIZE_OFFSET, name2.length);
     map.setChars(r, NAME_CHARS_OFFSET, name2, 0, name2.length);
 
-    // you can use a formatter to make the map easier to debug by using your own
-    // logic to convert records to strings
-    map.setFormatter(new Formatter() {
-      @Override
-      public String format(IPrimitiveAccess parent, int pointer) {
-        int age = parent.getInt(pointer, AGE_OFFSET);
-        int num = parent.getInt(pointer, NAME_SIZE_OFFSET);
-        char chars[] = new char[num];
-        parent.getChars(pointer, NAME_CHARS_OFFSET, chars, 0, num);
-        return String.format("%s, age %d%s", new String(chars), age, age < 18 ? " (underage!)" : "");
-      }
-    });
-
-    map.setDebug(DebugLevel.DEBUG_CONTENT);
     System.out.println(map);
     /**
      * Outputs :
@@ -108,7 +107,16 @@ public class FormattingMapElements {
      * 1002=Juliet, age 13 (underage!)
      * </pre>
      */
+  }
 
+  private static final class PersonFormatter implements Formatter {
+    @Override
+    public String format(IPrimitiveAccess parent, int pointer) {
+      int age = parent.getInt(pointer, AGE_OFFSET);
+      int num = parent.getInt(pointer, NAME_SIZE_OFFSET);
+      char chars[] = new char[num];
+      parent.getChars(pointer, NAME_CHARS_OFFSET, chars, 0, num);
+      return String.format("%s, age %d%s", new String(chars), age, age < 18 ? " (underage!)" : "");
+    }
   }
 }
-
