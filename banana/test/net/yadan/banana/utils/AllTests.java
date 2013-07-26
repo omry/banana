2ed41bdd66286ce66056fe78095403a5709c52ef
omry <omry@yadan.net>
2013-07-26 06:47:18 +0100

    .

---
+++ banana/test/net/yadan/banana/utils/AllTests.java
@@ -0,0 +1,16 @@
+package net.yadan.banana.utils;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+//@formatter:off
+@RunWith(Suite.class)
+@SuiteClasses({
+  LRUTest.class,
+  TextIndexTest.class,
+})
+//@formatter:on
+public class AllTests {
+
+}
